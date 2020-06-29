3b8a7fcb4a92e900ff0782d0c6d8f92350277a35
David Emanuel Vetter <dvetter@mpi.localnet>
2020-06-29 11:11:03 +0200

    Forgotten Test for Pair

---
+++ src/test/java/de/vetter/masterthesis/TestPair.java
@@ -0,0 +1,30 @@
+package de.vetter.masterthesis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPair {
+
+	@Test
+	public void testGetSetFirst() {
+		Pair<Integer, String> pair = new Pair<Integer, String>(12, "example");
+		assertEquals(12, (int) pair.getFirst());
+		
+		pair.setFirst(-34);
+		assertEquals(-34, (int) pair.getFirst());
+	}
+
+	@Test
+	public void testGetSetSecond() {
+		Pair<Integer, String> pair = new Pair<Integer, String>(12, "example");
+		assertEquals("example", pair.getSecond());
+		
+		pair.setFirst(-34);
+		assertEquals("example", pair.getSecond());
+		
+		pair.setSecond("new");
+		assertEquals("new", pair.getSecond());
+	}
+
+}
